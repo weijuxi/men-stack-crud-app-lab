bb2cc8b787b7f7bc74bbf90affc836eeae1a6c8b
weijuxi <2568706310@qq.com>
2024-09-08 11:33:42 -0700

    Add the food new and food show page

--- models/food.js
+++ models/food.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const foodSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    img: String,
 });
 
 const Food = mongoose.model("Food", foodSchema);
--- server.js
+++ server.js
@@ -12,14 +12,36 @@ mongoose.connection.on("connected", () => {
 });
 
 const Food = require("./models/food.js");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
 
 // GET /fruits/new  too add new food into the database
-app.get("/food/new", (req, res) => {
+app.get("/foods/new", async (req, res) => {
     res.render("foods/new.ejs");
+});
+
+// GET /fruits/:id  to display a specific food in the database
+app.get("/foods/:id", async (req, res) => {
+    const food = await Food.findById(req.params.id);
+    res.render("foods/show.ejs", { food });
+});
+
+// GET /fruits  to display all the food in the database
+app.get("/foods", async (req, res) => {
+    const allFoods = await Food.find();
+    res.render("foods/index.ejs", { foods: allFoods });
+});
+
+// POST /fruits  to create a new food in the database
+app.post("/foods", async (req, res) => {
+    if (!req.body.price) {
+        req.body.price = 0;
+    }
+    await Food.create(req.body);
+    res.redirect("/foods/new");
 });
 
 app.listen(3000, () => {
